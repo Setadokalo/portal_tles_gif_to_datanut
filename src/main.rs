c7bb88e2b76dc6d87778380e9ec086479e0f8c7a
Setadokalo <darkenchanter1@gmail.com>
2021-06-23 02:56:34 -0500

    Add somewhat-functional palette simplification

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,28 @@
-use std::{borrow::Borrow, collections::HashMap, fs::write};
+use std::{borrow::Borrow, collections::HashMap, env, error::Error, fmt::Display, fs::write};
 
 use gif::*;
 
-#[derive(PartialEq, Eq, Clone, Copy)]
+#[derive(Debug, PartialEq, Eq, Clone, Copy)]
 struct Color {
 	r: u8,
 	g: u8,
 	b: u8,
 }
 impl Color {
-	pub fn new(r: u8, g: u8, b: u8) -> Self {
+	fn new(r: u8, g: u8, b: u8) -> Self {
 		Color {r, g, b}
 	}
-	pub fn as_mixed(mut self, other: &Color) -> Self {
+	fn as_mixed(mut self, other: &Color) -> Self {
 		self.r = self.r / 2 + other.r / 2;
 		self.g = self.g / 2 + other.g / 2;
 		self.b = self.b / 2 + other.b / 2;
 
 		self
+	}
+	fn mix(&self, other: &Color) -> Self {
+		Self::new(self.r / 2 + other.r / 2,
+				self.g / 2 + other.g / 2,
+				self.b / 2 + other.b / 2)
 	}
 }
 
@@ -38,7 +43,7 @@ fn simplify_palette(palette_raw: impl IntoIterator<Item = impl Borrow<u8>>) -> (
 		let mut closest_hit_idx = 0;
 		for c_idx in 0..palette.len() {
 			let col = &palette[c_idx];
-			for nc_idx in c_idx..palette.len() {
+			for nc_idx in (c_idx+1)..palette.len() {
 				let ncol = &palette[nc_idx];
 				let color_distance = ((col.r as f64 - ncol.r as f64).powi(2) + (col.g as f64 - ncol.g as f64).powi(2) + (col.b as f64 - col.b as f64).powi(2)).sqrt();
 				if color_distance < closest_distance {
@@ -50,9 +55,20 @@ fn simplify_palette(palette_raw: impl IntoIterator<Item = impl Borrow<u8>>) -> (
 		}
 		let ccol_1 = palette[closest_index];
 		let ccol_2 = palette[closest_hit_idx];
+		println!("Combining colors {:?} and {:?} (indexes {} and {}) into {:?}", ccol_1, ccol_2, closest_index, closest_hit_idx, ccol_1.mix(&ccol_2));
 		palette[closest_index] = ccol_1.as_mixed(&ccol_2);
+		for (_, value) in cmap.iter_mut() {
+			if *value > closest_hit_idx {
+				*value -= 1;
+			}
+			if *value == closest_hit_idx {
+				*value = closest_index;
+			}
+		}
+		// cmap.insert(ccol_2.0, closest_index);
 		palette.remove(closest_hit_idx);
 	}
+	println!("Palette simplified to \nlength of {}, Map of source to target colors is {:?}", palette.len(), cmap);
 	let mut conv_palette = Vec::with_capacity(palette.len() * 3);
 	for color in palette {
 		conv_palette.push(color.r);
@@ -63,11 +79,10 @@ fn simplify_palette(palette_raw: impl IntoIterator<Item = impl Borrow<u8>>) -> (
 }
 
 fn palette_to_string<'a>(palette: impl IntoIterator<Item = impl Borrow<u8>>) -> (String, HashMap<usize, usize>) {
-	let palette = palette.into_iter();
-	let remap = HashMap::new();
-	let mut s = String::with_capacity(palette.size_hint().0);
-	for (idx, c_channel) in palette.enumerate() {
-		s += &format!("{}, ", c_channel.borrow());
+	let (palette, remap) = simplify_palette(palette);
+	let mut s = String::with_capacity(palette.len());
+	for (idx, c_channel) in palette.iter().enumerate() {
+		s += &format!("{}, ", c_channel);
 		// s += &format!("{:0>2x}", c_channel.borrow());
 
 		if idx == 63 * 3 + 2 {
@@ -88,9 +103,27 @@ const BASE_64: [char; 64] = [
 	'4', '5', '6', '7', '8', '9', '+', '/',
 ];
 
+// #[derive(Debug)]
+// struct DummyError {
+// 	reason: String
+// }
+// impl DummyError {
+// 	fn new(reason: impl Into<String>) -> Self {
+// 		Self { reason: reason.into() }
+// 	}
+// }
+
+// impl Display for DummyError {
+//     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+//       	self.reason.fmt(f)
+//     }
+// }
+
+// impl Error for DummyError {}
+
 fn main() -> anyhow::Result<()>{
 	let mut encoded: String;
-	let f = std::fs::File::open("convert.gif")?;
+	let f = std::fs::File::open(env::args().nth(2).unwrap_or("convert.gif".to_string()))?;
 	let mut d = Decoder::new(f)?;
 	let mut frames = Vec::new();
 	let mut local_palettes = false;
@@ -111,7 +144,7 @@ fn main() -> anyhow::Result<()>{
 		panic!("Local Palettes are not implemented");
 		// encoded = "L".to_string();
 	}
-
+	let mut f_cnt = 0;
 	for frame in frames {
 		// if local_palettes {
 		// 	if let Some(palette) = frame.palette {
@@ -120,18 +153,20 @@ fn main() -> anyhow::Result<()>{
 		// 		encoded += &g_palette_string;
 		// 	}
 		// }
+		f_cnt += 1;
 		for p in frame.buffer.iter() {
-			if *p > 63 {
-				eprintln!("GIF uses more than 64 palette entries! This does not encode properly! Clamping the invalid index to 63...");
+			let p = *palette_map.get(&(*p as usize)).expect("Palette did not contain entry");
+			if p > 63 {
+				panic!("GIF uses more than 64 palette entries! This does not encode properly! Clamping the invalid index to 63...");
 			}
-			encoded.push(BASE_64[(*p as usize).min(63)]);
+			encoded.push(BASE_64[p]);
 		}
 	}
-	encoded.push('"');
+	encoded += &format!("\"\nFRAME_COUNT <- {}", f_cnt);
 	// println!("{}", encoded);
 	if o_capacity != encoded.capacity() {
 		eprintln!("Capacity changed during runtime!")
 	}
-	write("data_export.nut", encoded)?;
+	write("data.nut", encoded)?;
 	Ok(())
 }
